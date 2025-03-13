d1bd0fa7613e87ebcca99e7c06447f1e079f53a9
Siim Laasnurm <siim.laasnurm@hotmail.com>
2025-03-13 21:56:34 +0200

    Initial

---
+++ src/test/java/com/siimlaasnurm/flightplanner/FlightPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siimlaasnurm.flightplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
